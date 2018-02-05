3d65cc41f23abe96d461e23606edc6992b730626
Levi Klingler <lklingler@covermymeds.com>
2018-02-05 14:58:52 -0500

    Working solution

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source "https://rubygems.org"
 
 gem 'httparty'
 gem 'sidekiq'
+gem 'rack'
 gem 'sinatra'
 gem 'pry'
--- client/sidekiq_workers.rb
+++ client/sidekiq_workers.rb
@@ -1,6 +1,13 @@
 require 'sidekiq'
 require_relative 'http_connection'
-Sidekiq::Logging.logger = nil
+
+Sidekiq.configure_client do |config|
+  config.redis = { db: 1 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { db: 1 }
+end
 
 class GetRequestSender
   include Sidekiq::Worker
@@ -9,10 +16,13 @@ class GetRequestSender
 
   def perform(path, params={})
     # For exercise 3, replace this comment with code that
-    # sends the request, parses the response, and uses `puts` to 
+    # sends the request, parses the response, and uses `puts` to
     # print the message part of the response
+    response = HttpConnection.get(path, { body: params })
+    puts JSON.parse(response.parsed_response)["message"]
 
     # For exercise 5, replace this comment with code that
     # retries the request if it fails
+    raise if response.code != 200
   end
 end
